0e59041cf635284c323c203c6d31e1ae4657b883
haygcao <ngokelcho@outlook.com>
2017-09-18 23:43:15 +0800

    Update fq.sh

--- fq.sh
+++ fq.sh
@@ -147,9 +147,9 @@ echo
 echo -e "\e[1;36m 下载sy618扶墙规则\e[0m"
 wget --no-check-certificate -q -O /tmp/sy618 https://raw.githubusercontent.com/sy618/hosts/master/dnsmasq/dnsfq
 echo
-#echo -e "\e[1;36m 下载racaljk规则\e[0m"
-#wget --no-check-certificate -q -O /tmp/racaljk https://raw.githubusercontent.com/racaljk/hosts/master/dnsmasq.conf
-#echo
+echo -e "\e[1;36m 下载googlehosts规则\e[0m"
+wget --no-check-certificate -q -O /tmp/racaljk https://raw.githubusercontent.com/googlehosts/hosts/master/hosts-files/dnsmasq.conf
+echo
 sleep 3
 #echo -e "\e[1;36m 删除racaljk规则中的冲突规则\e[0m"
 #sed -i '/google/d' /tmp/racaljk
@@ -159,11 +159,11 @@ echo -e "\e[1;36m 创建用户自定规则缓存\e[0m"
 cp /etc/dnsmasq.d/userlist /tmp/userlist
 echo
 echo -e "\e[1;36m 合并dnsmasq缓存\e[0m"
-#cat /tmp/userlist /tmp/racaljk /tmp/sy618 > /tmp/fq
-cat /tmp/userlist /tmp/sy618 > /tmp/fq
+cat /tmp/userlist /tmp/googlehosts /tmp/sy618 > /tmp/fq
+#cat /tmp/userlist /tmp/sy618 > /tmp/fq
 echo
 echo -e "\e[1;36m 删除dnsmasq临时文件\e[0m"
-rm -rf /tmp/userlist /tmp/sy618 #/tmp/racaljk
+rm -rf /tmp/userlist /tmp/sy618 /tmp/googlehosts
 echo
 echo -e "\e[1;36m 删除注释和本地规则\e[0m"
 sed -i '/#/d' /tmp/fq
@@ -176,7 +176,7 @@ echo "
 ## 【Copyright (c) 2014-2017, clion007】                          ##
 ##                                                                ##
 ## 感谢https://github.com/sy618/hosts                             ##
-## 感谢https://github.com/racaljk/hosts                           ##
+## 感谢https://github.com/googlehosts/hosts                       ##
 ####################################################################
 
 # Localhost (DO NOT REMOVE) Start
@@ -201,10 +201,10 @@ killall dnsmasq
 echo
 sleep 2
 echo -e "\e[1;36m 获取脚本更新脚本\e[0m"
-wget --no-check-certificate -q -O /etc/dnsmasq/fq_update.sh https://raw.githubusercontent.com/clion007/dnsmasq/master/fq_update.sh && chmod 755 /etc/dnsmasq/fq_update.sh
+wget --no-check-certificate -q -O /etc/dnsmasq/fq_update.sh https://raw.githubusercontent.com/haygcao/crosswall/master/fq_update.sh && chmod 755 /etc/dnsmasq/fq_update.sh
 echo
 echo -e "\e[1;36m 获取规则更新脚本\e[0m"
-wget --no-check-certificate -q -O /etc/dnsmasq/fqrules_update.sh https://raw.githubusercontent.com/clion007/dnsmasq/master/fqrules_update.sh && chmod 755 /etc/dnsmasq/fqrules_update.sh
+wget --no-check-certificate -q -O /etc/dnsmasq/fqrules_update.sh https://raw.githubusercontent.com/haygcao/crosswall/master/fqrules_update.sh && chmod 755 /etc/dnsmasq/fqrules_update.sh
 echo
 sleep 1
 grep "dnsmasq" $CRON_FILE >/dev/null
@@ -240,7 +240,7 @@ if [ ! $? -eq 0 ]; then
 	fi	
 fi
 echo -e "\e[1;36m 创建脚本更新检测副本\e[0m"
-wget --no-check-certificate -q -O /etc/dnsmasq/fq_auto.sh https://raw.githubusercontent.com/clion007/dnsmasq/master/fq_auto.sh
+wget --no-check-certificate -q -O /etc/dnsmasq/fq_auto.sh https://raw.githubusercontent.com/haygcao/crosswall/master/fq_auto.sh
 chmod 755 /etc/dnsmasq/fq_auto.sh
 echo
 clear
